9fd2c633fde6cf47f3eeac9ed08020d2bf332079
ARMAAN CHAHAL <132256505+armaan1chl@users.noreply.github.com>
2024-06-09 16:36:52 +0530

    Create module1.sol

---
+++ module1.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract AssertionExample {
+    function divide(uint256 _dividend, uint256 _divisor) external pure returns (uint256) {
+        // require statement
+        require(_divisor != 0, "Divisor cannot be zero.");
+
+        // assert statement
+        assert(_dividend >= _divisor);
+
+        // Perform the division
+        uint256 quotient = _dividend / _divisor;
+
+        // revert statement
+        if (quotient > 10) {
+            revert("Quotient cannot exceed 10.");
+        }
+
+        return quotient;
+    }
+}
